a813c557610e6b65acc43c878331630386886a0f
caudillo99 <diego.caudillo@uabc.edu.mx>
2020-03-23 22:47:53 -0700

    update: uppercase and special characters

--- Taller/getsE161.c
+++ Taller/getsE161.c
@@ -1,53 +1,106 @@
 #include<stdio.h>
 #include<conio.h>
-
+#include <stdlib.h>
+#define BYTE unsigned char
 struct keypad{
-    char key;
-    unsigned char qty;
-    unsigned char counter;
+    BYTE qty;
+    BYTE counter;
+    BYTE alt[5];
 };
 typedef struct keypad keypad_t;
-void getsE161( char *str );
 
+void getsE161( char *str );
+BYTE toUpper(BYTE c);
+BYTE toLower(BYTE c);
+void clear (void);
+void clearCounter(void);
+void backspace(void);
+
+keypad_t keyboard[13] = {
+        {1, 0, {'0'}},                {1, 0,{'1'}},                  {4, 0, {'2','A','B','C'}}, 
+        {4, 0, {'3','D','E','F'}},    {4, 0,{'4','G','H','I'}},      {4, 0, {'5','J','K','L'}}, 
+        {4, 0, {'6','M','N','O'}},    {5, 0,{'7','P','Q','R','S'}},  {4, 0, {'8','T','U','V'}}, 
+        {5, 0, {'9','W','X','Y','Z'}}, {1,0,{'*'}},{1,0,{'#'}}
+    }; 
 
 int main(int argc, char const *argv[]){ 
-    char str[80];
-    
+    char str[80] ;
     getsE161(str);
+    puts(">>>Mensaje final<<<");
+    puts(str);
+    
     return 0;
 }
 
-char getKey(void){
-    return ( kbhit() ? getch() : 0);
+/*Funcion que convierte de minusculas a mayusculas*/
+BYTE toUpper(BYTE c){
+    return (c >= 'a' && c <= 'z') ? c-32 : c;  
+}
+/*Funcion que convierte de mayusculas a minusculas*/
+BYTE toLower(BYTE c){
+    return (c >= 'A' && c <= 'Z') ? c+32 : c; 
 }
 
-/*
-DUDA: COMO ES LA VERIFICACION PARA ESCRIBIR EL SIGUIENTE CARACTER?
-SE TENDRA QUE PRESIONAR A(ADVANCE) CADA QUE SE QUIERA ESCRIBIR OTRO
-CARACTER O CUANDO SE PRESIONA OTRA TECLA DISTINTA A LA ANTERIORMENTE
-PRESIONADA, AVANZA A LA ESCRITURA DEL NUEVO CARACTER?
-*/
+/*Funcion que es teclado */
 void getsE161(char *str){
-    unsigned char i;
-    keypad_t keyboard[16] = {
-        {'1', 1, 0},{'2', 3, 0},{'3', 3, 0},{'A', 3, 0},
-        {'4', 3, 0},{'5', 3, 0},{'6', 3, 0},{'B', 3, 0},
-        {'7', 4, 0},{'8', 3, 0},{'9', 4, 0},{'C', 3, 0},
-        {'*', 0, 0},{'0', 0, 0},{'#', 0, 0},{'D', 3, 0}
-    }; 
-    char c;
+    BYTE i,key, upper = 0, aux, c;
+    const char *const str_aux = str;
     do{
-        c = getKey();
-        if(c){
-            for (i = 0; i < 16; i++){
-                if(c == keyboard[i].key){
-                    keyboard[i].counter++;
-                    break;
+        while ( kbhit() ){
+            key = getch(); /*Recibe la tecla pulsada por el usuario*/
+            if(toUpper(key) != 'A'){
+                /*Revisa en el arreglo de teclas si se encuentra la tecla pulsada*/
+                for(i = 0; i < 13; i++){
+                    if(key == keyboard[i].alt[0]){
+                         /*En caso de que se haya pulsado la mismca tecla 
+                        mas de una vez, se actualiza el nuevo caracter*/
+                        backspace();
+                        c = keyboard[i].alt[keyboard[i].counter];
+                        /*imprime en minusculas si la bandera es 0
+                        de lo contrario imprime en mayusculas*/
+                        if(!upper){
+                            putchar( toLower(c) );
+                            *str = toLower(keyboard[i].alt[keyboard[i].counter]);
+                        }else {
+                            putchar( toUpper(c) );
+                            *str = toUpper(keyboard[i].alt[keyboard[i].counter]);
+                        }
+                        /*Mientras el contador de caracteres que contiene cada tecla
+                        se menor, se aumenta el contador con el fin de mostrar el 
+                        siguiente caracter. Una vez que llega al maximo se reinicializa a 0*/
+                        keyboard[i].counter += (keyboard[i].counter<keyboard[i].qty)? 1 : -keyboard[i].qty;  
+                        break; 
+                    }
+                    switch ( toUpper(key) ){
+                        case 'B':    
+                            *str = 0;
+                            if(str_aux != str)
+                                str--;
+                            break;
+                        case 'C': upper = !upper;
+                            break;
+                        case 'D': key = 13;
+                            break;
+                    }
                 }
+            }else{
+                clearCounter();
+                putchar(' ');
+                str++;
             }
-            if(keyboard[i].counter){
-                putchar(keyboard[i].key);
-            }
+            aux = key;
         }
-    } while (c != 13);
+    } while (key != 13);
+    *str++ = '\0';
+}
+/*Funcion que reinicializa los contadores
+de la posicion de cada tecla*/
+void clearCounter(void){
+    for(BYTE i = 0; i<13 ; i++)
+        keyboard[i].counter = 0;
+}
+void backspace(void){
+    putchar(8);
+    putchar(' ');
+    putchar(8);
 }
