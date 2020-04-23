321911be036c2ace07f1ee74ad8f0796c2801c83
caudillo99 <diego.caudillo@uabc.edu.mx>
2020-04-22 21:41:53 -0700

    p7 delays and final report

--- practica7/delay/delay.asm
+++ practica7/delay/delay.asm
@@ -5,66 +5,72 @@
 .def B = R17
 .def C = R18
 .def D = R19
-main:
-	CALL delay1seg
-		ret
+;--------------init PORTS-----------------------------------
+	LDI R24, INIT_VALUE
+	OUT DDRB, R24
+
+;--------------MAIN LOOP---------------------------------
+NEXT:
+	;****Llamar retardo ****
+	;CALL DELAY103us
+	;CALL DELAY100ms
+	;CALL DELAY1seg
+	OUT PINB, R24
+	
+	RJMP NEXT
 	
 DELAY103us:
-	LDI A,182
-	delay1:
-		LDI B, 1
-	delay2:
-		DEC B
+		LDI 	A,182
+delay1:
+		LDI 	B, 1
+delay2:
+		DEC 	B
 		NOP             
-	
-		BRNE delay2
-
-		DEC A
+		BRNE 	delay2
+		DEC 	A
 		NOP
 		NOP
-		BRNE delay1
+		BRNE 	delay1
 		RET
 
 ;******************* Delay 100ms *******************
 DELAY100ms:
-	LDI		A,2
+	LDI 	A, 190
+J1:
+	LDI 	B, 122
+J2:
+	LDI		C, 11
+J3:	
 	NOP
-NXT1:
-	LDI 	B,246
-NXT2:
-	LDI 	C,203
-NXT3:
-	LDI 	D,3
-NXT4:
-	DEC 	D
 	NOP
-	BRNE 	NXT4
+	NOP
 	DEC 	C
-	BRNE 	NXT3
+	BRNE 	J3
 	DEC 	B
-	BRNE 	NXT2
-	DEC		A
-	BRNE	NXT1
+	BRNE 	J2
+	DEC 	A
+	BRNE 	J1
 	RET
 
 ;******************* Delay 1seg *******************
-delay1seg:
-    LDI A, 142
-jmp1:
-    LDI B, 112
-jmp2:
-    LDI C, 167
-jmp3:
+DELAY1seg:
+	LDI 	A, 241
+L1:	
+	LDI 	B, 1
+L2:	
+	LDI 	C, 48
+L3:	
+	LDI 	D, 230
+L4:	
 	NOP
 	NOP
 	NOP
-    DEC C
-    BRNE jmp3
-    
-	DEC B
-    BRNE jmp2
-    
-	DEC A
-    BRNE jmp1
-
+	DEC 	D
+	BRNE 	L4
+	DEC 	C
+	BRNE 	L3
+	DEC 	B 
+	BRNE 	L2
+	DEC 	A
+	BRNE 	L1
     RET
